74c525ee143031e1228fedb298172180dd9e5a5e
Johannes Eimer <johannes.eimer@jep-dev.com>
2021-11-25 16:36:07 +0100

    add test structure

---
+++ tests/integration/__init__.py
@@ -0,0 +1,18 @@
+class TestDHL:
+    def test_create_label_DE(self):
+        pass
+
+    def test_create_label_DE_Warensendung(self):
+        pass
+
+    def test_create_label_EU(self):
+        pass
+
+    def test_create_label_CH(self):
+        pass
+
+    def test_create_label_force(self):
+        pass
+
+    def test_create_label_wrong(self):
+        pass
